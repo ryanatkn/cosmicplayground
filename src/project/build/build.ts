68b03a96ac75a67ea0db0c5d16fe1b32d303bc15
Ryan Atkinson <ryanatkn@gmail.com>
2019-06-14 22:56:53 -0500

    rearrange build script

--- src/project/build/build.ts
+++ src/project/build/build.ts
@@ -35,7 +35,7 @@ import {clean} from './clean';
 import {plainCss} from './rollup-plugin-plain-css';
 import {sy} from './rollup-plugin-sy';
 
-const watch = argv['watch'];
+const {watch} = argv;
 
 // TODO These modules require `esModuleInterop` to work correctly.
 // Rather than doing that and forcing `allowSyntheticDefaultImports`,
@@ -43,135 +43,6 @@ const watch = argv['watch'];
 // This can probably be sorted out cleanly when `ts-node` supports ES modules.
 const resolve: typeof resolveFIXME.default = resolveFIXME as any;
 const commonjs: typeof commonjsFIXME.default = commonjsFIXME as any;
-
-const runBuild = async (): Promise<void> => {
-	verboseLog(ck.magenta(`building ... NODE_ENV=${NODE_ENV}`));
-
-	await clean();
-
-	// run rollup
-	if (watch) {
-		// TODO we currently have things a bit wonky between /build and /static - need to figure out how we want things to work (it can be nice to get prod builds in /build for dev purposes)
-		// copy over static files
-		verboseLog(ck.magenta('copying static files'));
-		await fs.copy(paths.appStatic, paths.appBuildDistClient, {
-			dereference: true,
-		});
-
-		// run the watcher
-		verboseLog(ck.magenta('building and watching'));
-		await runRollupWatcher();
-		verboseLog(ck.magenta('stopped watching'));
-	} else {
-		// build the js
-		verboseLog(ck.magenta('building'));
-		await runRollupBuild();
-		verboseLog(ck.magenta('completed build'));
-
-		// copy over static files
-		verboseLog(ck.magenta('copying static files'));
-		await fs.copy(paths.appStatic, paths.appBuildDistClient, {
-			dereference: true,
-		});
-	}
-};
-
-const runRollupWatcher = async (): Promise<void> => {
-	return new Promise((_resolve, reject) => {
-		const watchOptions = createWatchOptions();
-		verboseLog(ck.magenta('watchOptions'), watchOptions);
-		const watcher = rollup.watch([watchOptions]);
-
-		watcher.on('event', event => {
-			verboseLog(ck.magenta(`rollup event: ${event.code}`));
-			switch (event.code) {
-				case 'START': // the watcher is (re)starting
-				case 'BUNDLE_START': // building an individual bundle
-				case 'BUNDLE_END': // finished building a bundle
-				case 'END': // finished building all bundles
-					break;
-				case 'ERROR': // encountered an error while bundling
-					console.log('error', event);
-					reject(`Error: ${event.message}`);
-					break;
-				case 'FATAL': // encountered an unrecoverable error
-					console.log('fatal', event);
-					reject(`Fatal error: ${event.message}`);
-					break;
-				default:
-					throw Error(`Unknown rollup watch event code: ${event.code}`);
-			}
-		});
-
-		// call this ever?
-		// watcher.close();
-	});
-};
-
-const runRollupBuild = async (): Promise<{
-	build: RollupBuild;
-	output: RollupOutput;
-}> => {
-	const inputOptions = createInputOptions();
-	const outputOptions = createOutputOptions();
-
-	const build = await rollup.rollup(inputOptions);
-
-	const output = await build.generate(outputOptions);
-
-	// for (const chunkOrAsset of output.output) {
-	//   if (chunkOrAsset.isAsset) {
-	//     // For assets, this contains
-	//     // {
-	//     //   isAsset: true,                 // signifies that this is an asset
-	//     //   fileName: string,              // the asset file name
-	//     //   source: string | Buffer        // the asset source
-	//     // }
-	//     console.log('Asset', chunkOrAsset);
-	//   } else {
-	//     // For chunks, this contains
-	//     // {
-	//     //   code: string,                  // the generated JS code
-	//     //   dynamicImports: string[],      // external modules imported dynamically by the chunk
-	//     //   exports: string[],             // exported variable names
-	//     //   facadeModuleId: string | null, // the id of a module that this chunk corresponds to
-	//     //   fileName: string,              // the chunk file name
-	//     //   imports: string[],             // external modules imported statically by the chunk
-	//     //   isDynamicEntry: boolean,       // is this chunk a dynamic entry point
-	//     //   isEntry: boolean,              // is this chunk a static entry point
-	//     //   map: string | null,            // sourcemaps if present
-	//     //   modules: {                     // information about the modules in this chunk
-	//     //     [id: string]: {
-	//     //       renderedExports: string[]; // exported variable names that were included
-	//     //       removedExports: string[];  // exported variable names that were removed
-	//     //       renderedLength: number;    // the length of the remaining code in this module
-	//     //       originalLength: number;    // the original length of the code in this module
-	//     //     };
-	//     //   },
-	//     //   name: string                   // the name of this chunk as used in naming patterns
-	//     // }
-	//     console.log('Chunk', chunkOrAsset.modules);
-	//   }
-	// }
-
-	await build.write(outputOptions); // don't care about the output of this - maybe refactor
-
-	return {build, output};
-};
-
-const createWatchOptions = (): RollupWatchOptions => {
-	const watchOptions: RollupWatchOptions = {
-		...createInputOptions(),
-		output: createOutputOptions(),
-		watch: {
-			// chokidar,
-			clearScreen: false,
-			exclude: ['node_modules/**'],
-			// include,
-		},
-	};
-	return watchOptions;
-};
 
 const createInputOptions = (): InputOptions => {
 	const inputOptions: InputOptions = {
@@ -268,6 +139,135 @@ const createOutputOptions = (): OutputOptions => {
 		// strict
 	};
 	return outputOptions;
+};
+
+const createWatchOptions = (): RollupWatchOptions => {
+	const watchOptions: RollupWatchOptions = {
+		...createInputOptions(),
+		output: createOutputOptions(),
+		watch: {
+			// chokidar,
+			clearScreen: false,
+			exclude: ['node_modules/**'],
+			// include,
+		},
+	};
+	return watchOptions;
+};
+
+const runBuild = async (): Promise<void> => {
+	verboseLog(ck.magenta(`building ... NODE_ENV=${NODE_ENV}`));
+
+	await clean();
+
+	// run rollup
+	if (watch) {
+		// TODO we currently have things a bit wonky between /build and /static - need to figure out how we want things to work (it can be nice to get prod builds in /build for dev purposes)
+		// copy over static files
+		verboseLog(ck.magenta('copying static files'));
+		await fs.copy(paths.appStatic, paths.appBuildDistClient, {
+			dereference: true,
+		});
+
+		// run the watcher
+		verboseLog(ck.magenta('building and watching'));
+		await runRollupWatcher();
+		verboseLog(ck.magenta('stopped watching'));
+	} else {
+		// build the js
+		verboseLog(ck.magenta('building'));
+		await runRollupBuild();
+		verboseLog(ck.magenta('completed build'));
+
+		// copy over static files
+		verboseLog(ck.magenta('copying static files'));
+		await fs.copy(paths.appStatic, paths.appBuildDistClient, {
+			dereference: true,
+		});
+	}
+};
+
+const runRollupBuild = async (): Promise<{
+	build: RollupBuild;
+	output: RollupOutput;
+}> => {
+	const inputOptions = createInputOptions();
+	const outputOptions = createOutputOptions();
+
+	const build = await rollup.rollup(inputOptions);
+
+	const output = await build.generate(outputOptions);
+
+	// for (const chunkOrAsset of output.output) {
+	//   if (chunkOrAsset.isAsset) {
+	//     // For assets, this contains
+	//     // {
+	//     //   isAsset: true,                 // signifies that this is an asset
+	//     //   fileName: string,              // the asset file name
+	//     //   source: string | Buffer        // the asset source
+	//     // }
+	//     console.log('Asset', chunkOrAsset);
+	//   } else {
+	//     // For chunks, this contains
+	//     // {
+	//     //   code: string,                  // the generated JS code
+	//     //   dynamicImports: string[],      // external modules imported dynamically by the chunk
+	//     //   exports: string[],             // exported variable names
+	//     //   facadeModuleId: string | null, // the id of a module that this chunk corresponds to
+	//     //   fileName: string,              // the chunk file name
+	//     //   imports: string[],             // external modules imported statically by the chunk
+	//     //   isDynamicEntry: boolean,       // is this chunk a dynamic entry point
+	//     //   isEntry: boolean,              // is this chunk a static entry point
+	//     //   map: string | null,            // sourcemaps if present
+	//     //   modules: {                     // information about the modules in this chunk
+	//     //     [id: string]: {
+	//     //       renderedExports: string[]; // exported variable names that were included
+	//     //       removedExports: string[];  // exported variable names that were removed
+	//     //       renderedLength: number;    // the length of the remaining code in this module
+	//     //       originalLength: number;    // the original length of the code in this module
+	//     //     };
+	//     //   },
+	//     //   name: string                   // the name of this chunk as used in naming patterns
+	//     // }
+	//     console.log('Chunk', chunkOrAsset.modules);
+	//   }
+	// }
+
+	await build.write(outputOptions); // don't care about the output of this - maybe refactor
+
+	return {build, output};
+};
+
+const runRollupWatcher = async (): Promise<void> => {
+	return new Promise((_resolve, reject) => {
+		const watchOptions = createWatchOptions();
+		verboseLog(ck.magenta('watchOptions'), watchOptions);
+		const watcher = rollup.watch([watchOptions]);
+
+		watcher.on('event', event => {
+			verboseLog(ck.magenta(`rollup event: ${event.code}`));
+			switch (event.code) {
+				case 'START': // the watcher is (re)starting
+				case 'BUNDLE_START': // building an individual bundle
+				case 'BUNDLE_END': // finished building a bundle
+				case 'END': // finished building all bundles
+					break;
+				case 'ERROR': // encountered an error while bundling
+					console.log('error', event);
+					reject(`Error: ${event.message}`);
+					break;
+				case 'FATAL': // encountered an unrecoverable error
+					console.log('fatal', event);
+					reject(`Fatal error: ${event.message}`);
+					break;
+				default:
+					throw Error(`Unknown rollup watch event code: ${event.code}`);
+			}
+		});
+
+		// call this ever?
+		// watcher.close();
+	});
 };
 
 runBuild()
