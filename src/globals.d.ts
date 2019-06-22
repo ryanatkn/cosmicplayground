75ec260ebfb0064729b1a0174e2fa7815b9b801e
Ryan Atkinson <ryanatkn@gmail.com>
2019-06-22 01:23:14 -0500

    fix PartialExcept and PartialOnly types

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -4,9 +4,9 @@ type OmitStrict<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 // there are probably better names for them!
 // see `Required`, `Exclude` and `Extract` for possible leads for improvements
 type PartialExcept<T, K extends keyof T> = {[P in K]: T[P]} &
-	{[P in Exclude<keyof T, K>]?: T[Exclude<keyof T, K>]};
+	{[P in Exclude<keyof T, K>]?: T[P]};
 type PartialOnly<T, K extends keyof T> = {[P in K]?: T[P]} &
-	{[P in Exclude<keyof T, K>]: T[Exclude<keyof T, K>]};
+	{[P in Exclude<keyof T, K>]: T[P]};
 
 type PartialValues<T> = {
 	[P in keyof T]: Partial<T[P]>;
