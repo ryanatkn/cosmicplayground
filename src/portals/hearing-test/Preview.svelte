28089681358577f19108cd8ebec5516c305f0cd2
Ryan Atkinson <ryanatkn@gmail.com>
2020-09-11 10:11:09 -0500

    remove the hearing test warning

--- src/portals/hearing-test/Preview.svelte
+++ src/portals/hearing-test/Preview.svelte
@@ -4,14 +4,10 @@
 
 <div class="preview">
 	<h2>🐶 hearing test 🐶</h2>
-	<h3>🐾 🐕 beware ye, creature 🐕 🐾</h3>
 </div>
 
 <style>
 	.preview {
 		padding: var(--portal_padding);
-	}
-	h3 {
-		color: hsla(40deg, 60%, 65%, 1);
 	}
 </style>
