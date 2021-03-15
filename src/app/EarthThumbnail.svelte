429c003d69c0d060adb876dbde70cf2e66d38443
Ryan Atkinson <ryanatkn@gmail.com>
2021-03-15 09:42:18 -0600

    tweak the deep breath text color

--- src/app/EarthThumbnail.svelte
+++ src/app/EarthThumbnail.svelte
@@ -48,7 +48,7 @@
 	.text {
 		font-weight: 900;
 		white-space: nowrap;
-		color: var(--ocean_text_color);
+		color: var(--ocean_color); /* not using text color so it matches the base water color */
 		height: 100%;
 		width: 100%;
 		position: absolute;
