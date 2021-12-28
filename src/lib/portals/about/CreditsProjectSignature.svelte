87150659d8df6e421e4436d3c030db12e7871404
Ryan Atkinson <mail@ryanatkn.com>
2021-12-28 11:23:17 -0600

    update project signature

--- src/lib/portals/about/CreditsProjectSignature.svelte
+++ src/lib/portals/about/CreditsProjectSignature.svelte
@@ -1,28 +1,13 @@
 <div class="credits-project-signature">
-	<div class="item">
-		✉ <a href="mailto:void@cosmicplayground.org">void@cosmicplayground.org</a> &nbsp;&nbsp;
-	</div>
-	<div class="item">
-		<a href="https://twitter.com/cosmicplaygroun"
-			>@cosmicplaygroun <img
-				src="/assets/characters/twit.png"
-				alt="@cosmicplaygroun on Twitter"
-				class="w-16
-					h-16"
-				style="opacity: 0.8; margin-left: -8px;"
-			/>
-		</a>
-		&nbsp;
-		<a href="https://github.com/ryanatkn/cosmicplayground">
-			source code <img
-				src="/assets/characters/octocat.png"
-				alt="cosmicplayground source code on GitHub"
-				class="w-10
+	<a href="https://github.com/ryanatkn/cosmicplayground">
+		public domain source code <img
+			src="/assets/characters/octocat.png"
+			alt="cosmicplayground source code on GitHub"
+			class="w-10
 					h-10 ml-1"
-				style="opacity: 0.8; margin-right: 4px;"
-			/>
-		</a>
-	</div>
+			style="opacity: 0.8; margin-right: 4px;"
+		/>
+	</a>
 </div>
 
 <style>
@@ -31,9 +16,5 @@
 		justify-content: center;
 		align-items: center;
 		flex-wrap: wrap;
-	}
-	.item {
-		white-space: nowrap;
-		padding: 0 var(--spacing-3);
 	}
 </style>
