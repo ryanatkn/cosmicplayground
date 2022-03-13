2f72b9af9f7db404204f54b2150c1f40fc17ebb0
Ryan Atkinson <2608646+ryanatkn@users.noreply.github.com>
2022-03-13 17:13:36 -0600

    upgrade linter (#15)

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import {typescript} from 'svelte-preprocess-esbuild';
 import adapter from '@sveltejs/adapter-static';
 
-const dev = process.env.NODE_ENV !== 'production';
-
 /** @type {import('@sveltejs/kit').Config} */
 export default {
 	preprocess: typescript(),
@@ -12,13 +10,5 @@ export default {
 	kit: {
 		adapter: adapter(),
 		files: {assets: 'src/static'},
-		vite: {
-			ssr: {
-				noExternal: ['@feltcoop/felt'],
-			},
-			optimizeDeps: {
-				exclude: ['@feltcoop/felt'],
-			},
-		},
 	},
 };
