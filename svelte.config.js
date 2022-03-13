ab9e766d98b10144832babdb52b91c02d754ff19
Ryan Atkinson <mail@ryanatkn.com>
2022-03-13 18:05:44 -0500

    upgrade

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import {typescript} from 'svelte-preprocess-esbuild';
 import adapter from '@sveltejs/adapter-static';
 
-const dev = process.env.NODE_ENV !== 'production';
-
 /** @type {import('@sveltejs/kit').Config} */
 export default {
 	preprocess: typescript(),
@@ -13,9 +11,6 @@ export default {
 		adapter: adapter(),
 		files: {assets: 'src/static'},
 		vite: {
-			ssr: {
-				noExternal: ['@feltcoop/felt'],
-			},
 			optimizeDeps: {
 				exclude: ['@feltcoop/felt'],
 			},
