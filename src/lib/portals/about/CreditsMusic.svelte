b334dba29f53ac7d5ad041834bae74be1275c157
Ryan Atkinson <mail@ryanatkn.com>
2022-03-24 04:42:40 -0500

    add credits file

---
+++ src/lib/portals/about/CreditsMusic.svelte
@@ -0,0 +1,41 @@
+<h3>music by Alexander Nakarada</h3>
+<p>
+	<a href="https://www.serpentsoundstudios.com">serpentsoundstudios.com</a> ∙
+	<a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>
+</p>
+<div class="audio-file">
+	<a href="/assets/audio/Alexander_Nakarada__Spacey_Intro.mp3">"Spacey Intro"</a>
+	<audio controls src="/assets/audio/Alexander_Nakarada__Spacey_Intro.mp3" />
+</div>
+<div class="audio-file">
+	<a href="/assets/audio/Alexander_Nakarada__Spacey_Outro.mp3">"Spacey Outro"</a>
+	<audio controls src="/assets/audio/Alexander_Nakarada__Spacey_Outro.mp3" />
+</div>
+<div class="audio-file">
+	<a href="/assets/audio/Alexander_Nakarada__Futuristic_4.mp3">"Futuristic 4"</a>
+	<audio controls src="/assets/audio/Alexander_Nakarada__Futuristic_4.mp3" />
+</div>
+<div class="audio-file">
+	<a href="/assets/audio/Alexander_Nakarada__Futuristic_1.mp3">"Futuristic 1"</a>
+	<audio controls src="/assets/audio/Alexander_Nakarada__Futuristic_1.mp3" />
+</div>
+<div class="audio-file">
+	<a href="/assets/audio/Alexander_Nakarada__Winter.mp3">"Winter"</a>
+	<audio controls src="/assets/audio/Alexander_Nakarada__Winter.mp3" />
+</div>
+
+<style>
+	p {
+		/* TODO remove after using Felt styles */
+		margin-bottom: 10px;
+	}
+	.audio-file {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		padding: 5px 0;
+	}
+	audio {
+		margin-left: 20px;
+	}
+</style>
