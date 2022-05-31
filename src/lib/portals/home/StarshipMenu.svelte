b30106d1114ba9126830a7c5d0f1d1f924905ffa
Blit Wagon <blitwagon@gmail.com>
2022-05-31 07:34:11 -0500

    todo

--- src/lib/portals/home/StarshipMenu.svelte
+++ src/lib/portals/home/StarshipMenu.svelte
@@ -4,12 +4,23 @@
 	import Panel from '$lib/app/Panel.svelte';
 
 	export let exit: () => void;
+
+	// TODO BLOCK
+	/*
+
+	what should the API for the main menu be like?
+
+	maybe `MainDialogMenuEntry` or `MenuEntry` or something
+	could be a component that could be mounted anywhere,
+	and adds its contents to the main dialog menu
+
+	*/
 </script>
 
 <Panel>
 	<div class="markup">
 		<LinkPath>🌠</LinkPath>
 		<h1>?????</h1>
-		<button on:click={() => exit()}>exit</button>
+		<button on:click={() => exit()}>resume</button>
 	</div>
 </Panel>
