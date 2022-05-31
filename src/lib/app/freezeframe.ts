7a94506c54c3b11d8441a95a084867034aa06b48
Blit Wagon <blitwagon@gmail.com>
2022-05-31 13:47:18 -0500

    wip

--- src/lib/app/freezeframe.ts
+++ src/lib/app/freezeframe.ts
@@ -1,6 +1,7 @@
 import type {ActionReturn} from 'svelte/action';
 
 import {memoize} from '$lib/util/memoize';
+import {toImageDataUrl} from '$lib/util/dom';
 
 export type Options = boolean;
 
@@ -9,22 +10,14 @@ const dataUrlCache: Map<any, string> = new Map();
 let canvas: HTMLCanvasElement;
 const getCanvas = (): HTMLCanvasElement => canvas || (canvas = document.createElement('canvas'));
 
-const draw = (canvas: HTMLCanvasElement, img: HTMLImageElement): string => {
-	const ctx = canvas.getContext('2d');
-	if (!ctx) throw Error('failed to get canvas context');
-	const height = img.naturalHeight;
-	const width = img.naturalWidth;
-	canvas.width = width;
-	canvas.height = height;
-	ctx.drawImage(img, 0, 0);
-	return canvas.toDataURL();
-};
-
 export const freezeframe = (img: HTMLImageElement, freeze: Options): ActionReturn<Options> => {
 	// TODO BLOCK fix so this handles changes, at least on updates
 	const src = img.src;
 
-	const toDataUrl = () => memoize(dataUrlCache, src, () => draw(getCanvas(), img));
+	const toDataUrl = () => {
+		console.log(`toDataUrl src`, src);
+		return memoize(dataUrlCache, src, () => toImageDataUrl(getCanvas(), img));
+	};
 
 	if (freeze) {
 		img.src = toDataUrl();
