d6211b57b111ee550458a4447cf5f13fa4881c8c
Blit Wagon <blitwagon@gmail.com>
2022-06-10 19:33:06 -0500

    show controls

--- src/lib/portals/home/StarshipMenu.svelte
+++ src/lib/portals/home/StarshipMenu.svelte
@@ -21,15 +21,40 @@
 </script>
 
 <Panel>
-	<div class="markup">
-		<Breadcrumbs>🌠</Breadcrumbs>
-		<StarshipPreview
-			onClick={() => {
-				exit();
-				toggleStarshipMode();
-			}}
-			{starshipMode}
-		/>
-		<button on:click={() => exit()}>resume</button>
+	<div class="markup centered">
+		<section>
+			<Breadcrumbs>🌠</Breadcrumbs>
+			<StarshipPreview
+				onClick={() => {
+					exit();
+					toggleStarshipMode();
+				}}
+				{starshipMode}
+			/>
+		</section>
+		<section>
+			<h2>controls</h2>
+			<table>
+				<thead><th>key</th><th>action</th></thead><tr
+					><td><code>Escape</code></td><td>toggle main menu</td></tr
+				><tr><td><code>`</code></td><td>toggle clock</td></tr>
+			</table>
+		</section>
+		<section>
+			<button on:click={() => exit()}>resume</button>
+		</section>
 	</div>
 </Panel>
+
+<style>
+	/* TODO upstream */
+	td {
+		padding: var(--spacing_sm);
+	}
+	tr:hover {
+		background-color: var(--bg_2);
+	}
+	h2 {
+		text-align: center;
+	}
+</style>
