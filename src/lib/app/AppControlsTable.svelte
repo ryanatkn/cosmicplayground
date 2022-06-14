13dc23a2458015a890b6302727c253f37e697513
Blit Wagon <blitwagon@gmail.com>
2022-06-14 02:28:09 -0500

    refactor main menu

---
+++ src/lib/app/AppControlsTable.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import {page} from '$app/stores';
+
+	import Panel from '$lib/app/Panel.svelte';
+	import type {ClockStore} from '$lib/app/clock';
+
+	export let clock: ClockStore; // TODO or use context?
+
+	$: navLevel = $page.url.pathname === '/' ? 0 : $page.url.pathname.split('/').length - 1;
+</script>
+
+<Panel
+	><div class="markup centered">
+		<section>
+			<h2>controls</h2>
+			<table>
+				<thead><th>key</th><th>action</th></thead><slot /><tr
+					><td><code>Escape</code></td><td>toggle main menu</td></tr
+				><tr
+					><td><code>Backtick `</code></td><td
+						>toggle clock (is {#if $clock.running}running{:else}paused{/if})</td
+					></tr
+				><tr
+					><td><code>ctrl+Escape</code></td><td
+						>navigate upwards ({#if navLevel === 0}you're at the top{:else if navLevel === 1}there's
+							one level above you{:else}there's {navLevel} levels above you{/if})</td
+					></tr
+				>
+			</table>
+		</section>
+	</div>
+</Panel>
+
+<style>
+	h2 {
+		text-align: center;
+	}
+</style>
--- src/lib/portals/home/StarshipMenu.svelte
+++ src/lib/portals/home/StarshipMenu.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import {scale} from 'svelte/transition';
-	import {page} from '$app/stores';
 
 	import Panel from '$lib/app/Panel.svelte';
 	import StarshipPreview from '$lib/portals/home/Preview.svelte';
 	import type {ClockStore} from '$lib/app/clock';
 	import StarshipStageScore from '$lib/portals/home/StarshipStageScore.svelte';
 	import type {StarshipStageScores} from '$lib/portals/home/starshipStage';
+	import AppControlsTable from '$lib/app/AppControlsTable.svelte';
 
 	export let clock: ClockStore; // TODO or use context?
 	// TODO refactor
@@ -16,8 +16,6 @@
 	export let resetScores: (() => void) | undefined;
 	export let importScores: (() => void) | undefined;
 	export let scores: StarshipStageScores | undefined;
-
-	$: navLevel = $page.url.pathname === '/' ? 0 : $page.url.pathname.split('/').length - 1;
 </script>
 
 <div class="wrapper">
@@ -65,52 +63,17 @@
 			</Panel>
 		</div>
 	{/if}
-	<!-- TODO extract controls so they appear in the main app menu at all times, can hook into them -->
-	<Panel
-		><div class="markup centered">
-			<section>
-				<h2>controls</h2>
-				<table>
-					<thead><th>key</th><th>action</th></thead><tr
-						><td><code>Escape</code></td><td>toggle main menu</td></tr
-					><tr><td><code>Space</code></td><td>toggle starship game</td></tr><tr
-						><td><code>Backtick `</code></td><td
-							>toggle clock (is {#if $clock.running}running{:else}paused{/if})</td
-						></tr
-					><tr
-						><td><code>ctrl+Escape</code></td><td
-							>navigate upwards ({#if navLevel === 0}you're at the top{:else if navLevel === 1}there's
-								one level above you{:else}there's {navLevel} levels above you{/if})</td
-						></tr
-					>
-				</table>
-			</section>
-		</div>
-	</Panel>
+	<AppControlsTable {clock}
+		><tr><td><code>Space</code></td><td>toggle starship game</td></tr></AppControlsTable
+	>
 </div>
 
 <style>
 	.wrapper {
 		padding: 0 var(--spacing_sm);
 	}
-	/* TODO upstream */
-	td {
-		padding: var(--spacing_sm);
-	}
-	tr:hover {
-		background-color: var(--tint_light_1);
-	}
-	th {
-		text-align: left;
-		font-weight: 300;
-		font-size: var(--font_size_xl);
-		padding: var(--spacing_sm);
-	}
 	h2 {
 		text-align: center;
-	}
-	section {
-		margin-bottom: var(--spacing_xl7);
 	}
 	.starship-preview {
 		display: flex;
