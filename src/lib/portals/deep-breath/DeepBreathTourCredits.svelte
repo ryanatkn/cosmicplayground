8f220d3f62d752b95ee87a822d467c2432f26252
Blit Wagon <blitwagon@gmail.com>
2022-07-20 04:34:23 -0500

    fix layout

--- src/lib/portals/deep-breath/DeepBreathTourCredits.svelte
+++ src/lib/portals/deep-breath/DeepBreathTourCredits.svelte
@@ -74,6 +74,8 @@
 
 <style>
 	.tour-credits {
+		width: 100%;
+		height: 100%;
 		display: flex;
 		align-items: center;
 		justify-content: center;
--- src/lib/portals/deep-breath/DeepBreathTourTitle.svelte
+++ src/lib/portals/deep-breath/DeepBreathTourTitle.svelte
@@ -33,6 +33,8 @@
 
 <style>
 	.tour-title {
+		width: 100%;
+		height: 100%;
 		display: flex;
 		align-items: center;
 		justify-content: center;
