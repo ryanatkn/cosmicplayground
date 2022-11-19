d04e0bb46080cce56b54210658b677b24e4e981a
Ryan Atkinson <mail@ryanatkn.com>
2022-11-18 21:54:15 -0600

    no ssr

--- src/hooks.ts
+++
@@ -1,6 +0,0 @@
-import type {Handle} from '@sveltejs/kit';
-
-// disable SSR because Pixi doesn't like doing that
-export const handle: Handle = async ({event, resolve}) => {
-	return resolve(event, {ssr: false});
-};
