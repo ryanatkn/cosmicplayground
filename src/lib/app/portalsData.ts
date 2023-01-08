449a664b5958150bd7a6d0078efc65dd2919c645
Ryan Atkinson <mail@ryanatkn.com>
2023-01-08 08:11:37 -0700

    fix

--- src/lib/app/portalsData.ts
+++ src/lib/app/portalsData.ts
@@ -2,20 +2,20 @@ import type {PortalsData, PortalData} from '$lib/app/portal';
 
 // TODO Keep in sync with `svelte.config.js` until we figure out how to share data.
 // This is all necessary because of the dynamic root route `[...slug].svelte`.
-import HomePortalData from 'src/routes/data';
-import AboutPortalData from '../../routes/data';
-import GravityUnlockPortalData from '../../routes/data';
-import SoggyPlanetPortalData from '../../routes/data';
-import DeepBreathPortalData from '../../routes/data';
-import StarlitHammockPortalData from 'src/routes/starlit-hammock/data';
-import Easings2PortalData from 'src/routes/easings-2/data';
-import PaintFreqsPortalData from 'src/routes/paint-freqs/data';
-import Easings1PortalData from 'src/routes/easings-1/data';
-import HearingTestPortalData from 'src/routes/hearing-test/data';
-import UnderConstructionPortalData from 'src/routes/under-construction/data';
-import FreqSpeedsPortalData from 'src/routes/freq-speeds/data';
-import ClocksPortalData from 'src/routes/clocks/data';
-import FreqSpectaclePortalData from 'src/routes/freq-spectacle/data';
+import HomePortalData from '../../routes/data';
+import AboutPortalData from '../../routes/about/data';
+import GravityUnlockPortalData from '../../routes/gravity-unlock/data';
+import SoggyPlanetPortalData from '../../routes/soggy-planet/data';
+import DeepBreathPortalData from '../../routes/deep-breath/data';
+import StarlitHammockPortalData from '../../routes/starlit-hammock/data';
+import Easings2PortalData from '../../routes/easings-2/data';
+import PaintFreqsPortalData from '../../routes/paint-freqs/data';
+import Easings1PortalData from '../../routes/easings-1/data';
+import HearingTestPortalData from '../../routes/hearing-test/data';
+import UnderConstructionPortalData from '../../routes/under-construction/data';
+import FreqSpeedsPortalData from '../../routes/freq-speeds/data';
+import ClocksPortalData from '../../routes/clocks/data';
+import FreqSpectaclePortalData from '../../routes/freq-spectacle/data';
 
 const portals: PortalData[] = [
 	HomePortalData,
