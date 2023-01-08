1981eb566b26bdad03db0e4c2ad8fff8bfda3e9e
Ryan Atkinson <mail@ryanatkn.com>
2023-01-08 08:23:52 -0700

    add colors

--- src/routes/soggy-planet/constants.ts
+++ src/routes/soggy-planet/constants.ts
@@ -1,1 +1,15 @@
 export const THUMBNAIL_WIDTH_DEFAULT = 600;
+
+export const SHORE_COLOR_1 = '#dabc97';
+export const SHORE_COLOR_2 = '#caaa81';
+export const SHORE_COLOR_3 = '#bb996e';
+export const SHORE_COLOR_4 = '#ab895c';
+export const SHORE_COLOR_5 = '#977852';
+export const SHORE_COLOR_6 = '#856947';
+export const SHORE_COLOR_7 = '#765d3a';
+export const SHORE_COLOR_8 = '#684e2f';
+export const SHORE_COLOR_9 = '#594226';
+export const SHORE_COLOR_10 = '#4c381d';
+export const SHORE_COLOR_11 = '#48301d';
+export const SHORE_COLOR_12 = '#442a1d';
+export const SHORE_COLOR_13 = '#41241c';
