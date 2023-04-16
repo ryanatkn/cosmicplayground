331d687df9eaf2db712d6e6efffc791833933d2e
Ryan Atkinson <mail@ryanatkn.com>
2023-04-16 12:56:45 -0600

    update credits

--- src/routes/about/CreditsProjectSignature.svelte
+++ src/routes/about/CreditsProjectSignature.svelte
@@ -1,33 +1,7 @@
 <div class="credits-project-signature">
 	<div class="links">
-		<div class="item" style="padding: 0;">
-			<a class="clickable" href="https://twitter.com/cosmicplaygroun">
-				<img
-					src="/assets/characters/twit.png"
-					alt="@cosmicplaygroun on Twitter"
-					title="@cosmicplaygroun on Twitter"
-					width="64"
-					height="64"
-				/>
-			</a>
-		</div>
-		<div class="item">
-			<div class="centered">
-				<div>public ⚘ domain</div>
-				<div><a href="https://github.com/ryanatkn/cosmicplayground">source code</a></div>
-			</div>
-		</div>
-		<div class="item">
-			<a class="clickable" href="https://github.com/cosmicplayground">
-				<img
-					src="/assets/characters/octocat.png"
-					alt="@cosmicplayground on GitHub"
-					title="@cosmicplayground on GitHub"
-					width="40"
-					height="40"
-				/>
-			</a>
-		</div>
+		<div>public ⚘ domain</div>
+		<div><a href="https://github.com/ryanatkn/cosmicplayground">source code</a></div>
 	</div>
 </div>
 
@@ -42,19 +16,7 @@
 		display: flex;
 		justify-content: center;
 		align-items: center;
-		flex-wrap: wrap;
-	}
-	.item {
+		flex-direction: column;
 		white-space: nowrap;
-		padding: 0 var(--spacing-3);
-	}
-	.clickable img {
-		opacity: 0.75;
-	}
-	.clickable img:hover {
-		opacity: 0.9;
-	}
-	.clickable img:active {
-		opacity: 1;
 	}
 </style>
