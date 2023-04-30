4fc889373d247142786faec0f9bfb4f4ec6e4447
Ryan Atkinson <mail@ryanatkn.com>
2023-04-29 20:57:23 -0600

    refactor

--- src/lib/music/play_song.ts
+++ src/lib/music/play_song.ts
@@ -1,4 +1,4 @@
-import {get} from 'svelte/store';
+import {get, writable} from 'svelte/store';
 
 import type {AudioResource, ResourceStore} from '$lib/app/resource';
 import {toResourceStore} from '$lib/app/resource';
@@ -7,13 +7,18 @@ import type {Song} from '$lib/music/songs';
 
 let audio_key: symbol | undefined;
 
-export interface SongPlayState {
-	audio: HTMLAudioElement;
-	play: Promise<void>;
-	ended: Promise<unknown>;
-}
-
 const DEFAULT_VOLUME = 0.5; // TODO where?
+
+export interface SongPlayState {
+	song: Song;
+	volume: number;
+	audio: ResourceStore<AudioResource> | null;
+	$audio: AudioResource | null;
+	audio_el: HTMLAudioElement | null;
+	play: Promise<void> | null;
+	ended: Promise<unknown> | null;
+}
+export const playing_song = writable<SongPlayState | null>(null);
 
 // TODO extract an audio player store
 // TODO this API is not fun, resources should probably be stores
@@ -21,6 +26,16 @@ export const play_song = async (
 	song: Song,
 	volume: number = DEFAULT_VOLUME,
 ): Promise<SongPlayState | undefined> => {
+	// state gets mustated as the `play_song` function progresses
+	const state: SongPlayState = {
+		song,
+		volume,
+		audio: null,
+		$audio: null,
+		audio_el: null,
+		play: null,
+		ended: null,
+	};
 	const {url} = song;
 	// TODO is this the desired behavior? if playing already, just pause and abort?
 	let abort = false;
@@ -33,18 +48,18 @@ export const play_song = async (
 		audio = toResourceStore('audio', song.url) as ResourceStore<AudioResource>; // TODO type
 		audio_by_url.set(url, audio); // TODO improve API, maybe return a typed store from `addResource`
 	}
+	state.audio = audio;
 	const key = (audio_key = Symbol());
 	await audio.load();
-	if (audio_key !== key) return;
-	const $audio = get(audio);
+	if (audio_key !== key) return; // canceled
+	const $audio = (state.$audio = get(audio));
 	if (!$audio || $audio.status !== 'success' || !$audio.audio) {
 		console.error('Failed to load song'); // TODO handle failures better (Dialog error?)
 		return;
 	}
 	$audio.audio.volume = volume; // TODO where?
-	return {
-		audio: $audio.audio,
-		play: play_audio($audio.audio), // TODO do something with this before resolving?
-		ended: new Promise((r) => $audio.audio!.addEventListener('ended', r, {once: true})),
-	};
+	state.audio_el = $audio.audio;
+	state.play = play_audio($audio.audio); // TODO do something with this before resolving?
+	state.ended = new Promise((r) => $audio.audio!.addEventListener('ended', r, {once: true}));
+	return state;
 };
