f34d57895319bbc8aa35f3a14ea88ac1629a9fa3
Ryan Atkinson <mail@ryanatkn.com>
2023-07-02 21:35:38 -0600

    wip

--- src/lib/app/Surface.svelte
+++ src/lib/app/Surface.svelte
@@ -123,7 +123,7 @@
 				const delta = last_pinch_distance - distance;
 				const magnitude = Math.abs(delta / 0.33); // magic number for per-event deltas
 				const sensitivity = magnitude * (POINTER_ZOOM_SENSITIVITY - 1) + 1; // TODO super hacky
-				debugZoomCamera(delta, (x1 + x2) / 2, (y1 + y2) / 2, e.pointerId, sensitivity); // TODO is weird that `delta` is only for direction, see the API, merge with `sensitivity` probably
+				debugZoomCamera(delta, (x1 + x2) / 2, (y1 + y2) / 2, magnitude, sensitivity); // TODO is weird that `delta` is only for direction, see the API, merge with `sensitivity` probably
 			}
 			last_pinch_distance = distance;
 		}
