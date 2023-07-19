b0e0d0ecda483affa6b482c77908fe31726130b6
Ryan Atkinson <mail@ryanatkn.com>
2023-07-19 17:42:00 -0600

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import {sveltekit} from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
-	optimizeDeps: {
-		exclude: ['@feltjs/felt-ui', '@feltcoop/dealt'],
-	},
+	ssr: {noExternal: ['@feltjs/felt-ui', '@feltcoop/dealt']},
 	build: {
 		chunkSizeWarningLimit: 550,
 	},
