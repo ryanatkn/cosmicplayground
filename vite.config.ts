c2e3e56a01c9b1472e03d6ec005bcc67a841368c
Ryan Atkinson <mail@ryanatkn.com>
2023-09-30 23:53:55 -0600

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import {sveltekit} from '@sveltejs/kit/vite';
 
 const config: UserConfig = {
 	plugins: [sveltekit()],
-	// TODO BLOCK remove this?
-	ssr: {noExternal: ['@fuz.dev/fuz']},
 };
 
 export default config;
