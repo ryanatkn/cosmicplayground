bb05ed68dff7ac7f914de514f2c2ed381b04ea3d
Ryan Atkinson <mail@ryanatkn.com>
2023-10-04 12:18:22 -0600

    deployment

--- _app/immutable/chunks/Preview.b7d7d828.js
+++
@@ -1,9 +0,0 @@
-import{n as V,e as u,s as q,x as v,b as d,l as C,g as D,c as E,y as g,d as f,m as t,V as I,p as b,i as G,o as r,a as x}from"./scheduler.37029f49.js";import{S as M,i as S}from"./index.13406d9d.js";function H(P){let e,l,y="cosmicplayground",h,s,m,i,k,p,a,w,_;return{c(){e=u("div"),l=u("h1"),l.textContent=y,h=q(),s=u("div"),m=v(`about
-		`),i=u("img"),p=v(`
-		credits
-		`),a=u("img"),_=v(`
-		contact`),this.h()},l(o){e=d(o,"DIV",{class:!0});var n=C(e);l=d(n,"H1",{class:!0,"data-svelte-h":!0}),D(l)!=="svelte-1f4kun"&&(l.textContent=y),h=E(n),s=d(n,"DIV",{class:!0});var c=C(s);m=g(c,`about
-		`),i=d(c,"IMG",{src:!0,alt:!0,width:!0,height:!0}),p=g(c,`
-		credits
-		`),a=d(c,"IMG",{src:!0,alt:!0,class:!0,width:!0,height:!0}),_=g(c,`
-		contact`),c.forEach(f),n.forEach(f),this.h()},h(){t(l,"class","svelte-1skkop5"),I(i.src,k="/assets/icon.png")||t(i,"src",k),t(i,"alt","cosmicplayground icon"),t(i,"width","32"),t(i,"height","32"),b(i,"margin","10px"),I(a.src,w="/assets/icon.png")||t(a,"src",w),t(a,"alt","cosmicplayground icon"),t(a,"class","mirror_x"),t(a,"width","32"),t(a,"height","32"),b(a,"margin","10px"),t(s,"class","info svelte-1skkop5"),t(e,"class","preview svelte-1skkop5")},m(o,n){G(o,e,n),r(e,l),r(e,h),r(e,s),r(s,m),r(s,i),r(s,p),r(s,a),r(s,_)},p:x,i:x,o:x,d(o){o&&f(e)}}}class A extends M{constructor(e){super(),S(this,e,null,H,V,{})}}export{A as P};
