a9106d2ffc77c86fabf5d5f42fec637cbac40285
Ryan Atkinson <mail@ryanatkn.com>
2023-10-16 19:43:51 -0600

    todo

--- gro.config.ts
+++ gro.config.ts
@@ -1,6 +1,7 @@
 import type {CreateGroConfig} from '@grogarden/gro';
 
 const config: CreateGroConfig = async (cfg) => {
+	// TODO disable the library build more cleanly than this
 	cfg.map_package_json = (pkg) => {
 		pkg.exports = undefined;
 		return pkg;
