7ebdcbf13cce94350c2a4188bfebee55f89da927
Ryan Atkinson <mail@ryanatkn.com>
2023-10-31 18:25:07 -0600

    fix import

--- gro.config.ts
+++ gro.config.ts
@@ -1,6 +1,6 @@
-import type {CreateGroConfig} from '@grogarden/gro';
+import type {Create_Gro_Config} from '@grogarden/gro';
 
-const config: CreateGroConfig = async (cfg) => {
+const config: Create_Gro_Config = async (cfg) => {
 	// TODO disable the library build more cleanly than this
 	cfg.map_package_json = (pkg) => {
 		pkg.exports = undefined;
