18960666c690ae07b5ae356be3ba1e6e2ca61425
Ryan Atkinson <mail@ryanatkn.com>
2024-04-12 23:46:03 -0600

    wip

--- src/lib/AppControlsTable.svelte
+++ src/lib/AppControlsTable.svelte
@@ -14,20 +14,25 @@
 		<section>
 			<h2 class="mb_lg">controls</h2>
 			<table>
-				<thead><th>key</th><th>action</th></thead><slot />
-				<tr><td><code>[Escape]</code></td><td>toggle main menu</td></tr>
-				<tr>
-					<td><code>[Backtick `]</code></td><td
-						>toggle clock (is {#if $clock.running}running{:else}paused{/if})</td
-					>
-				</tr>
-				<tr>
-					<td><code>[shift+Escape]</code></td><td
-						>navigate upwards ({#if navLevel === 0}you're at the top{:else if navLevel === 1}there's
-							one level above you{:else}there's {navLevel} levels above you{/if})</td
-					>
-				</tr>
-				<slot name="end" />
+				<thead>
+					<tr><th>key</th><th>action</th></tr>
+				</thead>
+				<tbody>
+					<slot />
+					<tr><td><code>[Escape]</code></td><td>toggle main menu</td></tr>
+					<tr>
+						<td><code>[Backtick `]</code></td><td
+							>toggle clock (is {#if $clock.running}running{:else}paused{/if})</td
+						>
+					</tr>
+					<tr>
+						<td><code>[shift+Escape]</code></td><td
+							>navigate upwards ({#if navLevel === 0}you're at the top{:else if navLevel === 1}there's
+								one level above you{:else}there's {navLevel} levels above you{/if})</td
+						>
+					</tr>
+					<slot name="end" />
+				</tbody>
 			</table>
 		</section>
 	</div>
