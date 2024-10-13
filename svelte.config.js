6adaad9529d8e4ed575cbcccd045a90be609e50f
Ryan Atkinson <mail@ryanatkn.com>
2024-10-13 05:13:17 -0600

    wip

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ export default {
 	compilerOptions: {runes: true},
 	// TODO ideally this would use the default but it conflicts with ctrl+shift+c in Chrome,
 	// but it's strange to me that I'm resetting it to what the Mac default is
-	vitePlugin: {inspector: {toggleKeyCombo: 'meta-shift'}}, // docs: https://github.com/sveltejs/vite-plugin-svelte/blob/main/docs/inspector.md
+	// vitePlugin: {inspector: {toggleKeyCombo: 'meta-shift'}}, // docs: https://github.com/sveltejs/vite-plugin-svelte/blob/main/docs/inspector.md
 	kit: {
 		adapter: adapter(),
 		paths: {relative: false}, // use root-absolute paths: https://kit.svelte.dev/docs/configuration#paths
